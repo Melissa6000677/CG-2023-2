9506ee33fd683ff87046447c4fe41f40626603f8
Melissa6000677 <141768909+Melissa6000677@users.noreply.github.com>
2023-09-19 15:44:12 -0500

    Versión 3
    Finalizado

--- c02/silla/silla.js
+++ c02/silla/silla.js
@@ -1,11 +1,14 @@
 // Constantes de medidas
-const anchoCuboBase = 2;
-const altoCuboBase = 2;
-const profundidadCuboBase = 2;
 const altoPatas = 1.0;   // Altura de las patas
 const anchoPatas = 0.1;  // Ancho de las patas
-const anchoSilla = 0.8;  // Ancho del asiento cuadrado
+let anchoSilla = 1;      // Ancho del asiento cuadrado
 const altoSilla = 0.2;   // Espesor del asiento y del respaldo
+const radioRespaldo = 0.4; // Radio del respaldo cilíndrico
+const espesorRespaldo = 0.2; // Espesor del respaldo cilíndrico
+
+// Otras constantes de medidas
+const radioCilindro = 0.4;
+const espesorCilindro = 0.2;
 
 // Configuración de la escena
 const scene = new THREE.Scene();
@@ -28,30 +31,35 @@ function crearCubo(ancho, alto, profundidad) {
     return new THREE.Mesh(geometry, material);
 }
 
+// Función para crear un cilindro con radio y altura personalizados
+function crearCilindro(radio, altura) {
+    const geometry = new THREE.CylinderGeometry(radio, radio, altura, 32);
+    const material = new THREE.MeshNormalMaterial();
+    return new THREE.Mesh(geometry, material);
+}
+
 // Crea el cubo base centrado alrededor del origen
-const cuboBase = crearCubo(anchoCuboBase, altoCuboBase, profundidadCuboBase);
+const cuboBase = crearCubo(anchoSilla, altoSilla, anchoSilla);
 // Mueve el asiento encima de las patas
-cuboBase.position.y = (0.5);
+cuboBase.position.y = (0.6);
 
-// Escala el cubo base para formar el asiento cuadrado
-cuboBase.scale.set(anchoSilla / anchoCuboBase, altoSilla / altoCuboBase, anchoSilla / profundidadCuboBase);
+// Crea las patas como en el código original
+const pata1 = crearCubo(anchoPatas, altoPatas, anchoPatas);
+pata1.position.set(-0.4, 0, -0.4);
 
-// Crea las patas escalando el cubo base
-const pata1 = cuboBase.clone();
-pata1.scale.set(anchoPatas, altoPatas, anchoPatas);
-pata1.position.set(-anchoSilla / 2, -altoPatas / 2, -anchoSilla / 2);
+const pata2 = crearCubo(anchoPatas, altoPatas, anchoPatas);
+pata2.position.set(0.4, 0, -0.4);
 
-const pata2 = cuboBase.clone();
-pata2.scale.set(anchoPatas, altoPatas, anchoPatas);
-pata2.position.set(anchoSilla / 2, -altoPatas / 2, -anchoSilla / 2);
+const pata3 = crearCubo(anchoPatas, altoPatas, anchoPatas);
+pata3.position.set(-0.4, 0, 0.4);
 
-const pata3 = cuboBase.clone();
-pata3.scale.set(anchoPatas, altoPatas, anchoPatas);
-pata3.position.set(-anchoSilla / 2, -altoPatas / 2, anchoSilla / 2);
+const pata4 = crearCubo(anchoPatas, altoPatas, anchoPatas);
+pata4.position.set(0.4, 0, 0.4);
 
-const pata4 = cuboBase.clone();
-pata4.scale.set(anchoPatas, altoPatas, anchoPatas);
-pata4.position.set(anchoSilla / 2, -altoPatas / 2, anchoSilla / 2);
+// Crea el cilindro encima del asiento y rota 90 grados en el eje Y
+const cilindroRespaldo = crearCilindro(radioRespaldo, espesorRespaldo);
+cilindroRespaldo.rotation.x = Math.PI / 2; // Rotación de 90 grados en Y
+cilindroRespaldo.position.set(0, 1.1, 0.4);
 
 // Agrega todos los elementos a la escena
 scene.add(cuboBase);
@@ -59,13 +67,34 @@ scene.add(pata1);
 scene.add(pata2);
 scene.add(pata3);
 scene.add(pata4);
+scene.add(cilindroRespaldo);
 
-// Función para animar la escena
+// Crea un grupo para la silla y agrega todos los elementos
+const grupoSilla = new THREE.Group();
+grupoSilla.add(cuboBase);
+grupoSilla.add(pata1);
+grupoSilla.add(pata2);
+grupoSilla.add(pata3);
+grupoSilla.add(pata4);
+grupoSilla.add(cilindroRespaldo);
+
+// Agrega el grupo a la escena
+scene.add(grupoSilla);
+
+// Función para animar la escena y rotar la silla
 const animate = () => {
     requestAnimationFrame(animate);
+
+    // Rotación de la silla
+    grupoSilla.rotation.y += 0.005; // Puedes ajustar la velocidad de rotación aquí
+
     renderer.render(scene, camera);
 };
 
 // Inicia la animación
 animate();
+
+
+
+
 
