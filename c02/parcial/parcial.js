438cf717812d03931a2a949c5f2b983eb153e6dc
Melissa6000677 <141768909+Melissa6000677@users.noreply.github.com>
2023-10-10 15:11:14 -0500

    Versión 2
    Creación de función cubo, con sus vértices y caras, función animar

--- c02/parcial/parcial.js
+++ c02/parcial/parcial.js
@@ -9,9 +9,45 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Posición de la cámara
-camera.position.z = 5;
-camera.position.x = 2;
-camera.position.y = 2;
+function cubo(x, y, z, escala, textureUrl) {
+  const textureLoader = new THREE.TextureLoader();
+  const texture = textureLoader.load(textureUrl);
 
+  // Crear geometria
+  const geometry = new THREE.Geometry();
+
+  // Definir los vértices del cubo
+  geometry.vertices.push(
+    new THREE.Vector3(-0.5, -0.5, -0.5),
+    new THREE.Vector3(0.5, -0.5, -0.5),
+    new THREE.Vector3(0.5, -0.5, 0.5),
+    new THREE.Vector3(-0.5, -0.5, 0.5),
+    new THREE.Vector3(-0.5, 0.5, -0.5),
+    new THREE.Vector3(0.5, 0.5, -0.5),
+    new THREE.Vector3(0.5, 0.5, 0.5),
+    new THREE.Vector3(-0.5, 0.5, 0.5)
+  );
+  // Definir las caras del cubo
+  geometry.faces.push(
+    new THREE.Face3(0, 1, 5),
+    new THREE.Face3(0, 5, 4),
+    new THREE.Face3(1, 2, 6),
+    new THREE.Face3(1, 6, 5),
+    new THREE.Face3(2, 3, 7),
+    new THREE.Face3(2, 7, 6),
+    new THREE.Face3(3, 0, 4),
+    new THREE.Face3(3, 4, 7),
+    new THREE.Face3(4, 5, 6),
+    new THREE.Face3(4, 6, 7),
+    new THREE.Face3(0, 3, 2),
+    new THREE.Face3(0, 2, 1)
+  );
+  // Calcular normales para las caras
+  geometry.computeFaceNormals();
+
+// Funcion animar
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
 animate();
