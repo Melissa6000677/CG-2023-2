afe600eb4cc38fa62ea2dd3d5a8a820eb009b215
Melissa6000677 <141768909+Melissa6000677@users.noreply.github.com>
2023-10-10 15:20:20 -0500

    Versión Final
    Código terminado

--- c02/parcial/parcial.js
+++ c02/parcial/parcial.js
@@ -45,6 +45,45 @@ function cubo(x, y, z, escala, textureUrl) {
   // Calcular normales para las caras
   geometry.computeFaceNormals();
 
+  // Calcular coordenadas UV manualmente
+  geometry.faceVertexUvs[0] = [];
+  for (let i = 0; i < geometry.faces.length; i++) {
+    const face = geometry.faces[i];
+    const uvs = [];
+    for (let j = 0; j < 3; j++) {
+      const vertexIndex = face.a + (j % 3);
+      const vertex = geometry.vertices[vertexIndex];
+      uvs.push(new THREE.Vector2((vertex.x + 0.5), (vertex.y + 0.5)));
+    }
+    geometry.faceVertexUvs[0].push(uvs);
+  }
+
+  const material = new THREE.MeshBasicMaterial({ wireframe: false, side: THREE.DoubleSide, map: texture });
+  const customMesh = new THREE.Mesh(geometry, material);
+  customMesh.position.set(x, y, z); // Posición del cubo
+  customMesh.scale.set(escala, escala, escala); // Transformacion de escala
+  scene.add(customMesh); // Agregar el cubo a la escena
+}
+
+// Función para trazar ejes
+function trazarEjes() {
+  const axesHelper = new THREE.AxesHelper(5); // Longitud de los ejes
+  scene.add(axesHelper);
+}
+
+// Posición de la cámara
+camera.position.z = 5;
+camera.position.x = 2;
+camera.position.y = 2;
+
+// Crea tres cubos 
+cubo(0, 0, 0, 1, 'https://static.vecteezy.com/system/resources/thumbnails/002/697/104/original/animated-green-pixel-grass-background-the-concept-of-games-background-squares-pattern-background-minecraft-concept-illustration-light-green-abstract-textured-polygonal-background-video.jpg');
+cubo(0, 0.75, 0, 0.5, 'https://static.vecteezy.com/system/resources/thumbnails/002/697/104/original/animated-green-pixel-grass-background-the-concept-of-games-background-squares-pattern-background-minecraft-concept-illustration-light-green-abstract-textured-polygonal-background-video.jpg');
+cubo(0, 1.125, 0, 0.25, 'https://static.vecteezy.com/system/resources/thumbnails/002/697/104/original/animated-green-pixel-grass-background-the-concept-of-games-background-squares-pattern-background-minecraft-concept-illustration-light-green-abstract-textured-polygonal-background-video.jpg');
+
+// Llama a la función para trazar ejes
+trazarEjes();
+
 // Funcion animar
 function animate() {
   requestAnimationFrame(animate);
